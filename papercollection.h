b8a7f28dd79a72ec9754459f428242621ea68c5a
ytken <59825228+ytken@users.noreply.github.com>
2020-10-05 11:17:39 +0300

    Update papercollection.h

--- papercollection.h
+++ papercollection.h
@@ -1,11 +1,32 @@
 #ifndef PAPERCOLLECTION_H
 #define PAPERCOLLECTION_H
+#include "wastepaper.h"
 
 
 class PaperCollection
 {
-public:
+ public:
     PaperCollection();
+    PaperCollection(const PaperCollection&);
+    void push(const WastePaper &paper);
+    WastePaper &pop(int pos);
+    void insert(int pos, const WastePaper &paper);
+    WastePaper &get(int pos) const;
+    void set(int pos, const WastePaper &paper);
+    int getCount() const;
+    ~PaperCollection();
+    bool isTheStackSustainable() const;
+
+ private:
+    struct PaperElement {
+        WastePaper paper;
+        PaperElement *next;
+        PaperElement();
+        PaperElement(const WastePaper&);
+    };
+    PaperElement *first;
+    int count;
+    int xCentre=0, yCentre=0, zCentre=0; //координаты центра тяжести
 };
 
 #endif // PAPERCOLLECTION_H
